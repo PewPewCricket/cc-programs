cebde64cf50c7f89604b5cd7d7516396028bf68a
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2026-07-05 13:22:54 -0500

    Add files via upload

---
+++ loadz/loadz.lua
@@ -0,0 +1,22 @@
+local LibDeflate = require("LibDeflate")
+
+local p = {}
+
+function p.loadz(chunk, name, mode, env)
+    return load(LibDeflate:DecompressDeflate(chunk), name, mode, env)
+end
+
+function p.loadfilez(path, mode, env)
+    local file = io.open(path, "r")
+    local data = file:read("*a")
+    if not data then error("failed to read " .. path) end
+    local func = p.loadz(data, "@"..path, mode, env)
+    file:close()
+    return func
+end
+
+function p.dofilez(path)
+    return p.loadfilez(path, "bt", _ENV)()
+end
+
+return p
