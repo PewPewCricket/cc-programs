bb65fcac939b925e7da3f4b9e63d25617cc736a9
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2026-07-19 14:32:16 -0500

    safety third or whatever

--- libzutils/libzutils.lua
+++ libzutils/libzutils.lua
@@ -1,12 +1,15 @@
+local expect = require("cc.expect")
 local LibDeflate = require("LibDeflate")
 
 local p = {}
 
 function p.loadz(chunk, name, mode, env)
+    expect(1, chunk, "string")
     return load(LibDeflate:DecompressDeflate(chunk), name, mode, env)
 end
 
 function p.loadfilez(path, mode, env)
+    expect(1, path, "string")
     local file = io.open(path, "r")
     local data = file:read("*a")
     if not data then error("failed to read " .. path) end
@@ -16,16 +19,19 @@ function p.loadfilez(path, mode, env)
 end
 
 function p.dofilez(path)
+    expect(1, path, "string")
     return p.loadfilez(path, "bt", _ENV)()
 end
 
 function p.serializez(t)
+    expect(1, t, "table")
     return LibDeflate:CompressDeflate(textutils.serialize(t))
 end
 
 p.serialisez = p.serializez
 
 function p.unserializez(s)
+    expect(1, s, "string")
     return textutils.unserialize(LibDeflate:DecompressDeflate(s))
 end
 
