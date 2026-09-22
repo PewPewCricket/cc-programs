d3b557fd9d2279730ec79fca0a1b3643ec07f59f
PewPewCricket <114350431+PewPewCricket@users.noreply.github.com>
2026-09-22 17:46:56 -0500

    Stright up packing my bytes

--- libcert/libcert.lua
+++ libcert/libcert.lua
@@ -1,12 +1,10 @@
 local expect = require("cc.expect")
 local base64 = require("cc.base64")
+local crypto = peripheral.find("cryptographic_accelerator")
+if not crypto then
+    error("libcert requires a cyptographic accelerator to run.")
+    end
 
--- Globals
-local dbg_log = function() return end
---local dbg_log = print
-local trustedFileName = "trusted"
-
--- Settings
 settings.define("libcert.certdir", {
     description = "Directory containing certificates.",
     default = "/.cert",
@@ -14,205 +12,193 @@ settings.define("libcert.certdir", {
 })
 
 local certdir = settings.get("libcert.certdir")
+local trustedFileName = "trusted"
+--                   HDR VER ISR SUB KEY FRM TO  FLG
+local certPartFmt = "c7  H   s2  s2  s2  d   d   B   "
+--                   HDR VER ISR SUB KEY FRM TO  FLG SIG
+local certFullFmt = "c7  H   s2  s2  s2  d   d   B   c64"
 
--- Check for peripherals
-local crypto = peripheral.find("cryptographic_accelerator")
-if not crypto then
-    error("libcert requires a cyptographic accelerator to run.")
-end
-
--- Library state
+local certMagic = "cc-cert"
 local p = {}
 p._VERSION = 1
 
--- Library functions
-p.create = function(issuer, subject, pubKey, 
-    isSigner, from, to)
-    
+p.create = function(issuer, subject, pubKey, from, to, isSigner)
     expect(1, issuer, "string")
     expect(2, subject, "string")
     expect(3, pubKey, "string")
-    expect(4, isSigner, "boolean")
-    
-    local cert = {}
-    local data = {
-        version = p._VERSION, -- version number
-        issuer = issuer,      -- issuer subj. id
-        subject = subject,    -- subject
-        key = pubKey,         -- pub. key of subj.
-        isSigner = isSigner,  -- can sign certs?
-        from = from,          -- from date in utc
-        to = to               -- to date in utc
-    }
-    
-    cert.data = textutils.serialize(data)
+    expect(4, from, "number")
+    expect(5, to, "number")
+    expect(6, isSigner, "boolean")
+
+    -- Flag Byte: 0, 0, 0, 0, 0, 0, 0, isSigner
+    local flagByte = 0
+    flagByte = bit32.bor(flagByte, bit32.lshift(isSigner and 1 or 0, 0))
+    if not from then from = 0 end
+    if not to then to = 0 end
+
+    local certPart = string.pack(certPartFmt, certMagic, p._VERSION, issuer, subject, pubKey, from, to, flagByte)
+    return certPart
+end
+
+p.sign = function(certPart, key)
+    expect(1, certPart, "string")
+    expect(2, key, "string")
+
+    local sig = crypto.sign(certPart, key)
+    local cert = certPart .. string.pack("c64", sig)
+
     return cert
 end
 
-p.sign = function(cert, key)
-    expect(1, cert, "table")
-    expect(2, key, "string")
+p.unpack = function(cert)
+    expect(1, cert, "string")
 
-    cert.sig = crypto.sign(cert.data, key)
-end
+    -- Flag Byte: 0, 0, 0, 0, 0, 0, 0, isSigner
+    local ok, magic, version, issuer, subject, key, from, to, flags, sig = pcall(string.unpack, certFullFmt, cert)
+    if not ok or magic ~= "cc-cert" or version ~= p._VERSION or to < from or from < 0 or to < 0 then
+        return nil
+    end
 
-p.verifyStructure = function(cert)
-    expect(1, cert, "table")
+    local isSigner = bit32.band(flags, bit32.rshift(1, 0)) ~= 0
 
-    if cert.data == nil or cert.sig == nil then
-        return false
-    end
-    
-    local certData = textutils.unserialize(cert.data)
-    if certData == nil then
-        return false
-    end
-    
-    if certData.version == nil or certData.issuer == nil
-        or certData.subject == nil or certData.key == nil
-        or certData.isSigner == nil or certData.from == nil
-        or certData.to == nil then
-        return false
-    end
-    
-    return true
+    return issuer, subject, key, from, to, sig, isSigner
 end
 
 p.save = function(cert, overwrite)
-    expect(1, cert, "table")
-    
-    if not p.verifyStructure(cert) then
-        error("malformed certificate.")
-    end
-    
-    local certData = textutils.unserialize(cert.data)
+    expect(1, cert, "string")
 
+    local ok, subject = p.unpack(cert)
     if overwrite == nil then overwrite = false end
-    local certpath = ("%s/%s.cert"):format(
-        certdir, base64.encode(certData.subject, "-_"))
+
+    if not ok then
+        error("malformed certificate")
+    end
+
+    local certpath = ("%s/%s.cert"):format(certdir, base64.encode(subject, "-_"))
     if not overwrite then
         if fs.exists(certpath) then
             error(certpath.." already exists.")
         end
     end
-    
+
     local file = io.open(certpath, "w")
-    file:write(textutils.serialize(cert))
+    file:write(cert)
     file:close()
 end
 
 p.load = function(subject)
     expect(1, subject, "string")
 
-    local certpath = ("%s/%s.cert"):format(
-        certdir, base64.encode(subject, "-_"))
+    local certpath = ("%s/%s.cert"):format(certdir, base64.encode(subject, "-_"))
     if not fs.exists(certpath) then
         return nil
     end
-    
+
     local file = io.open(certpath, "r")
-    local cert = textutils.unserialize(
-        file:read("*a"))
-    
-    if not p.verifyStructure(cert) then
+    local cert = file:read("*a")
+    file:close()
+
+    local ok = p.unpack(cert)
+    if not ok then
         error("malformed certificate")
     end
-    
+
     return cert
 end
 
 p.setTrust = function(cert, bool)
-    expect(1, cert, "table")
+    expect(1, cert, "string")
     expect(2, bool, "boolean")
-    
-    local trustedFile = io.open(("%s/%s"):format(
-        certdir, trustedFileName), "r")
+
     local trustedList = {}
+    local trustedPath = ("%s/%s"):format(certdir, trustedFileName)
+
+    local trustedFile = io.open(trustedPath, "r")
     if trustedFile then
-        trustedList = textutils.unserialize(
-            trustedFile:read("*a"))
+        trustedList = textutils.unserialize(trustedFile:read("*a"))
         trustedFile:close()
     end
-    
-    trustedList[crypto.sha256(cert.data)] = bool
-    
-    trustedFile = io.open(("%s/%s"):format(
-        certdir, trustedFileName), "w")
+
+    trustedList[crypto.sha256(cert)] = bool
+
+    trustedFile = io.open(trustedPath, "w")
     trustedFile:write(textutils.serialize(trustedList))
     trustedFile:close()
 end
 
 p.verify = function(cert, depth)
-    expect(1, cert, "table")
-    depth = depth or 0
-    if depth > 25 then
-        error("certificate chains may not exceed 25 signers.")
+    expect(1, cert, "string")
+
+    depth = (depth or 0) + 1
+    if depth > 32 then
+        error("certificate chains may not exceed 32 signers.")
     end
-    
-    if not p.verifyStructure(cert) then
+
+    local issuer, subject, _, from, to, sig = p.unpack(cert)
+    if not issuer then
         return false
     end
-    
-    local certData = textutils.unserialize(cert.data)
-    local trustedFile = io.open(("%s/%s"):format(
-        certdir, trustedFileName), "r")
-        
+
+    local trustedFile = io.open(("%s/%s"):format(certdir, trustedFileName), "r")
     if trustedFile then
-        local trustedList = textutils.unserialize(
-            trustedFile:read("*a"))
+        local trustedList = textutils.unserialize(trustedFile:read("*a"))
         trustedFile:close()
 
-        if trustedList[crypto.sha256(cert.data)] == true then
-            dbg_log(certData.subject..": trusted")
+        if trustedList[crypto.sha256(cert)] == true then
             return true
         end
     end
-    
-    local issuer = certData.issuer
-    if certData.subject == issuer then
-        dbg_log(certData.subject..": self signed")
+
+    if subject == issuer then
         return false
     end
-    
-    local issuerCert = p.load(issuer)
-    if not p.verify(issuerCert, depth) then
-        dbg_log(certData.subject..": bad issuer")
+
+    local icert = p.load(issuer)
+    if not p.verify(icert, depth) then
         return false
     end
-    
-    local issuerCertData =
-        textutils.unserialize(issuerCert.data)
-    if not issuerCertData.isSigner then
-        dbg_log(certData.subject..": issuer no sign")
+
+    local _, _, ikey, _, _, _, isParentSigner = p.unpack(icert)
+    if not isParentSigner then
         return false
     end
-    
+
     local curtime = os.epoch("utc")
-    if not (certData.from == 0 and certData.to == 0) then
-        if curtime > certData.to or curtime < certData.from
-            or certData.to < certData.from then
-            dbg_log(certData.subject..": bad date")
+    if not (from == 0 and to == 0) then
+        if curtime > to or curtime < from or to < from then
             return false
         end
     end
-    
-    if crypto.verify(cert.data, cert.sig, 
-        issuerCertData.key) then
-        dbg_log(certData.subject..": issuer ok")
+
+    local certPayload = cert:sub(1, -65)
+
+    if crypto.verify(certPayload, sig, ikey) then
         return true
     else
-        dbg_log(certData.subject..": bad signature")
         return false
     end
 end
 
 p.hasIssuer = function(cert)
-    local certData = textutils.unserialize(cert.data)
-    if not p.load(certData.issuer) then
-        return false, certData.issuer
+    expect(1, cert, "string")
+
+    local issuer = p.unpack(cert)
+    if not issuer then
+        return nil
+    end
+
+    if not p.load(issuer) then
+        return false, issuer
     else
         return true
     end
+end
+
+p.getKey = function(cert)
+    expect(1, cert, "string")
+
+    local _, _, key= p.unpack(cert)
+    return key
 end
 
 return p
